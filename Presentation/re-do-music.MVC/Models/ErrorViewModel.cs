95b8af3f587ef765a4955829050a73d6fdd07ea5
k-celal <celal51kara@gmail.com>
2023-10-22 12:54:44 +0300

    [Feat] Project Created
    The project was created in general terms

---
+++ Presentation/re-do-music.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace re_do_music.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
