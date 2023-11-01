db897ad2d5565a9c41cecdbd8241fecff6e8af0b
k-celal <celal51kara@gmail.com>
2023-11-02 00:21:47 +0300

    [Feat] Admin & User authentication
    Admin and user permissions set to pages and features

--- Presentation/re-do-music.MVC/Controllers/BrandController.cs
+++ Presentation/re-do-music.MVC/Controllers/BrandController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ReDoMusic.Domain.Entites;
 using ReDoMusic.Persistance.Contexts;
 
 namespace re_do_music.MVC.Controllers
 {
+
     public class BrandController : Controller
     {
         private readonly ReDoMusicDbContext _context;
@@ -19,12 +21,13 @@ namespace re_do_music.MVC.Controllers
             var brands = _context.Brands.ToList();
             return View(brands);
         }
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public IActionResult AddBrand()
         {
             return View();
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         public IActionResult AddBrand(string brandName, string brandDisplayText, string brandAddress)
         {
@@ -43,7 +46,7 @@ namespace re_do_music.MVC.Controllers
 
             return View();
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public IActionResult DeleteBrand(string id)
         {
