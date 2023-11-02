527ef41824c98edc97378647c7a534de623e9eee
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-11-02 15:32:20 +0300

    [Feat] Create Order Entity

---
+++ Core/ReDoMusic.Domain/Entites/Order.cs
@@ -0,0 +1,19 @@
+﻿using ReDoMusic.Domain.Common;
+using ReDoMusic.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDoMusic.Domain.Entites
+{
+    public class Order : EntityBase<Guid>
+    {
+        
+        public string ShippingAddress { get; set; }
+        public Payment PaymentMethod { get; set; }
+        public DateTime OrderDate { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
